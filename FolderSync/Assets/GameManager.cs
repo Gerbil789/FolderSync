54167602549e995656d6030532040a84bbe90422
Gerbil789 <67593297+Gerbil789@users.noreply.github.com>
2024-03-31 20:03:55 +0200

    update

--- FolderSync/Assets/GameManager.cs
+++ FolderSync/Assets/GameManager.cs
@@ -6,34 +6,34 @@ public class GameManager : MonoBehaviour
 {
     private async void Start()
     {
-        var localFolderPath = @"C:\Users\vojta\Documents\SyncFolder";
+        //var localFolderPath = @"C:\Users\vojta\Documents\SyncFolder";
 
-        var credentials = await Drive.GetCredentials(Application.dataPath + "/credentials.json");
-        if(credentials == null)
-        {
-            Debug.LogError("Failed to get credentials");
-            return;
-        }
+        //var credentials = await Drive.GetCredentials(Application.dataPath + "/credentials.json");
+        //if(credentials == null)
+        //{
+        //    Debug.LogError("Failed to get credentials");
+        //    return;
+        //}
 
 
-        var service = await Drive.InitializeDriveService(credentials);
-        if(service == null)
-        {
-            Debug.LogError("Failed to initialize drive service");
-            return;
-        }
+        //var service = await Drive.InitializeDriveService(credentials);
+        //if(service == null)
+        //{
+        //    Debug.LogError("Failed to initialize drive service");
+        //    return;
+        //}
 
-        var folderId = await Drive.GetFolderId(service, "Test");
-        if(folderId == null)
-        {
-            Debug.LogError("Failed to get folder id");
-            return;
-        }
+        //var folderId = await Drive.GetFolderId(service, "Test");
+        //if(folderId == null)
+        //{
+        //    Debug.LogError("Failed to get folder id");
+        //    return;
+        //}
 
-        Debug.Log("Folder ID: " + folderId);
+        //Debug.Log("Folder ID: " + folderId);
 
-        var version = await Drive.GetFolderVersion(service, folderId);
-        Debug.Log("Folder version: " + version);
+        //var version = await Drive.GetFolderVersion(service, folderId);
+        //Debug.Log("Folder version: " + version);
 
         //var url = @"https://drive.google.com/drive/folders/" + folderId;
         //Application.OpenURL(url);
